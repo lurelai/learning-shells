8baea61381a5fb6c3e5e391e8129a8551c99d205
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-24 20:21:15 -0300

    basic of variables

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+variable_type="not a pointer"
+proof=$variable_type
+
+echo origin: \"$proof\"
+
+proof="see it"
+
+echo Look, now, its values is \"$proof\" and the variable \"variable type\" stay the same: \"$variable_type\"
+
