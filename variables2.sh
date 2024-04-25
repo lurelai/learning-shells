280f1d1b46d73618102821022df6b3b362f76a9e
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-24 22:20:33 -0300

    that way look more interesting

--- variables2.sh
+++ variables2.sh
@@ -3,8 +3,9 @@
 number1=10
 number2=2
 
-echo $number1 plus $number2 equals $((number1+number2))
-echo $number1 minus $number2 equals $((number1-number2))
-echo $number1 times $number2 equals $((number1*number2))
-echo $number1 divided by $number2 equals $((number1/number2))
+echo "\
+$number1 plus $number2 equals $((number1+number2))
+$number1 minus $number2 equals $((number1-number2))
+$number1 times $number2 equals $((number1*number2))
+$number1 divided by $number2 equals $((number1/number2))"
 
