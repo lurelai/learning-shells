263a21f5632feb5efea8ee4f128a82984a4f1674
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-24 22:12:44 -0300

    basic of variables (2)

---
+++ variables2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+number1=10
+number2=2
+
+echo $number1 plus $number2 equals $((number1+number2))
+echo $number1 minus $number2 equals $((number1-number2))
+echo $number1 times $number2 equals $((number1*number2))
+echo $number1 divided by $number2 equals $((number1/number2))
+
