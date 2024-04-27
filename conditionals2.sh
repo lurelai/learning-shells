a8a82e601fddffa95963819b5578651d94064aa3
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-26 21:42:36 -0300

    conditionals

---
+++ conditionals2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+read -p "type one number: " n1
+read -p "type another number: " n2
+
+# Form 1
+echo -e "First form"
+if [ $(( $n1 % 2 )) = 0 ] && [ $(( $n2 % 2 )) = 0 ]; 
+then
+	echo all of $n1 and $n2 are an even numbers
+fi
+echo -e "\n"
+
+
+echo -e "Second form"
+# Form 2
+# It is legacy, the flag "-a" probaly will have suport for a long time but the "Form 3" is better then it
+if [ $(( $n1 % 2 )) = 0 -a $(( $n2 % 2 )) = 0 ];
+then
+	echo all of $n1 and $n2 are even numbers
+fi
+echo -e "\n"
+
+
+echo -e "Third form"
+# Form 3
+# It only works in the mordern shells, I tried it with the sh but it don't work
+# Be atenttion, the second "[" needs to be together with the first "["
+if [[ $(( $n1 % 2 )) == 0 && $(( $n2 % 2 )) == 0 ]];
+then
+	echo all of $n1 and $n2 are even numbers
+fi
+
